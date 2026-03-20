e3c0fbce65a8eb29f7aaae1211cdedf98853d5e9
Alexander <alexander.angatkin@gmail.com>
2026-03-20 19:38:25 +0300

    5

---
+++ app/src/main/java/com/example/angatkinmirea/StopwatchActivity.kt
@@ -0,0 +1,89 @@
+package com.example.angatkinmirea
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startForegroundService
+import com.example.angatkinmirea.ui.theme.AngatkinMIREATheme
+
+class StopwatchActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AngatkinMIREATheme {
+                StopwatchScreen(this)
+            }
+        }
+    }
+}
+
+@Composable
+fun StopwatchScreen(context: Context) {
+    RequestNotificationPermission(context)
+    val seconds by StopwatchService.secondsFlow.collectAsState()
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "$seconds", fontSize = 80.sp)
+
+        Spacer(modifier = Modifier.width(60.dp))
+
+        Button(onClick = {
+            val intent = Intent(context, StopwatchService::class.java).apply {
+                action = StopwatchService.ACTION_START
+            }
+            startForegroundService(context, intent)
+        }) {
+            Text("Старт")
+        }
+
+        Spacer(modifier = Modifier.width(30.dp))
+
+        Button(onClick = {
+            val intent = Intent(context, StopwatchService::class.java).apply {
+                action = StopwatchService.ACTION_PAUSE
+            }
+            context.startService(intent)
+        }) {
+            Text("Стоп")
+        }
+    }
+}
+
+@Composable
+fun RequestNotificationPermission(context: Context) {
+    LaunchedEffect(Unit) {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+            1
+        )
+    }
+}
---
+++ app/src/main/java/com/example/angatkinmirea/StopwatchService.kt
@@ -0,0 +1,104 @@
+package com.example.angatkinmirea
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+object NotificationHelper {
+    private const val CHANNEL_ID = "Task5"
+    private const val CHANNEL_NAME = "Stopwatch"
+
+    fun createNotificationChannel(context: Context) {
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_LOW)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    fun createMessage(context: Context, text: String) =
+        NotificationCompat.Builder(context,CHANNEL_ID)
+            .setSmallIcon(R.drawable.circle)
+            .setContentTitle("Секундомер работает")
+            .setContentText(text)
+            .build()
+}
+
+class StopwatchService : Service() {
+    private var seconds = 0
+    private var isRunning = false
+    private val SERVICE_ID = 100
+    private val serviceScope = CoroutineScope(Dispatchers.Default)
+    private var timerJob: Job? = null
+
+    companion object {
+        const val ACTION_START = "ACTION_START"
+        const val ACTION_PAUSE = "ACTION_PAUSE"
+        val secondsFlow = MutableStateFlow(0)
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return Binder()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("RRR", "onCreate()")
+    }
+
+    private fun startStopwatch() {
+        if (timerJob != null) return
+        timerJob = serviceScope.launch {
+            while (true) {
+                delay(1000)
+                seconds++
+                secondsFlow.value = seconds
+                updateNotification()
+                Log.d("TimerService", "Seconds: $seconds")
+            }
+        }
+    }
+
+    private fun pauseStopwatch() {
+        timerJob?.cancel()
+        timerJob = null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        NotificationHelper.createNotificationChannel(this)
+        super.startForeground(SERVICE_ID, NotificationHelper.createMessage(this, "Прошло $seconds секунд"))
+        when (intent?.action) {
+            ACTION_START -> startStopwatch()
+            ACTION_PAUSE -> pauseStopwatch()
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        timerJob?.cancel()
+        timerJob = null
+        super.onDestroy()
+    }
+
+    private fun updateNotification() {
+        val notification = NotificationCompat.Builder(this, "Task5")
+            .setSmallIcon(R.drawable.circle)
+            .setContentTitle("Секундомер работает")
+            .setContentText("Прошло $seconds секунд")
+            .build()
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(SERVICE_ID, notification)
+    }
+}
